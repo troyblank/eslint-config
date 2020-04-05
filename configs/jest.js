6457811395c2742a0926f7443d9f2cb1eba39d27
Troy Blank <troy@troyblank.com>
2020-04-05 09:19:52 -0500

    added jest globals to extend

---
+++ configs/jest.js
@@ -0,0 +1,12 @@
+module.exports = {
+    globals: {
+        after: true,
+        afterEach: true,
+        before: true,
+        beforeEach: true,
+        describe: true,
+        expect: true,
+        jest: true,
+        test: true
+    }
+};
